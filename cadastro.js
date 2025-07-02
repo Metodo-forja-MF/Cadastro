a73bc3fffb8f602f8a1931c0810c0749fe02d75f
MF <bertiwesley15@gmail.com>
2025-07-02 09:25:12 -0300

    cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -2,13 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_AUTH_DOMAIN",
-    projectId: "SEU_PROJECT_ID",
-    storageBucket: "SEU_BUCKET",
-    messagingSenderId: "SEU_MSG_ID",
-    appId: "SEU_APP_ID"
+  apiKey: "AIzaSyBKtKrRP4AjLHcdeEdyTlTZC9hHdv5y7no",
+  authDomain: "metodo-forja.firebaseapp.com",
+  databaseURL: "https://metodo-forja-default-rtdb.firebaseio.com",
+  projectId: "metodo-forja",
+  storageBucket: "metodo-forja.firebasestorage.app",
+  messagingSenderId: "949719386034",
+  appId: "1:949719386034:web:5c015b5fbe082821ea86ab",
+  measurementId: "G-6Z9EJLY5LZ"
 };
 
 const app = initializeApp(firebaseConfig);
