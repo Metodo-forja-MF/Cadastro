81fe9714e9e81c0bfd03e0993258b64764c1c83d
MF <bertiwesley15@gmail.com>
2025-07-02 09:27:48 -0300

    Add files via upload

--- cadastro.js
+++ cadastro.js
@@ -1,17 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBKtKrRP4AjLHcdeEdyTlTZC9hHdv5y7no",
-  authDomain: "metodo-forja.firebaseapp.com",
-  databaseURL: "https://metodo-forja-default-rtdb.firebaseio.com",
-  projectId: "metodo-forja",
-  storageBucket: "metodo-forja.firebasestorage.app",
-  messagingSenderId: "949719386034",
-  appId: "1:949719386034:web:5c015b5fbe082821ea86ab",
-  measurementId: "G-6Z9EJLY5LZ"
+    apiKey: "SUA_API_KEY",
+    authDomain: "SEU_AUTH_DOMAIN",
+    projectId: "SEU_PROJECT_ID",
+    storageBucket: "SEU_BUCKET",
+    messagingSenderId: "SEU_MSG_ID",
+    appId: "SEU_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -29,8 +26,8 @@ document.getElementById("cadastroForm").addEventListener("submit", async (e) => 
     const codigoRef = doc(db, "codigos", codigo);
     const codigoSnap = await getDoc(codigoRef);
 
-    if (!codigoSnap.exists()) {
-        alert("Código de verificação inválido!");
+    if (!codigoSnap.exists() || codigoSnap.data().usado) {
+        alert("Código de verificação inválido ou já utilizado!");
         return;
     }
 
@@ -41,6 +38,11 @@ document.getElementById("cadastroForm").addEventListener("submit", async (e) => 
             email: email,
             codigo: codigo
         });
+
+        await updateDoc(codigoRef, {
+            usado: true
+        });
+
         window.location.href = "https://metodo-forja-mf.github.io/Login/";
     } catch (error) {
         alert("Erro ao cadastrar: " + error.message);
